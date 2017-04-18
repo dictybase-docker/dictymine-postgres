77b495e0d5d833aaca29ac26fae3331af9c89e9b
Siddhartha Basu <siddhartha-basu@northwestern.edu>
2017-04-18 10:23:27 -0500

    Added creation of intermine specific database and users

--- 01_create_intermine_users.sh
+++ 01_create_intermine_users.sh
@@ -1,18 +1,39 @@
 #!/bin/bash
 set -e
 
-create_chado_user() {
-    if [ "${CHADO_USER+defined}" -a "${CHADO_PASS+defined}" -a "${CHADO_DB+defined}" ]
+create_intermine_role_and_db() {
+    if [ "${INTEMINE_USER+defined}" -a "${INTERMINE_PASSWORD+defined}" -a "${INTERMINE_DB+defined}" ]
     then
         psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" <<-EOSQL
-            CREATE ROLE $CHADO_USER WITH CREATEDB LOGIN ENCRYPTED PASSWORD '$CHADO_PASS';
-            CREATE DATABASE $CHADO_DB OWNER $CHADO_USER;
+            CREATE ROLE $INTERMINE_USER WITH CREATEDB LOGIN ENCRYPTED PASSWORD '$INTERMINE_PASS';
+            CREATE DATABASE $INTERMINE_DB OWNER $INTERMINE_USER;
 EOSQL
     fi
 }
 
+create_intermine_items_db() {
+    if [ "${INTEMINE_USER+defined}" -a "${INTERMINE_PASSWORD+defined}" -a "${INTERMINE_ITEMS_DB+defined}" ]
+    then
+        psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" <<-EOSQL
+            CREATE DATABASE $INTERMINE_ITEMS_DB OWNER $INTERMINE_USER;
+EOSQL
+    fi
+}
+
+create_intermine_userprofile_db() {
+    if [ "${INTEMINE_USER+defined}" -a "${INTERMINE_PASSWORD+defined}" -a "${INTERMINE_USERS_DB+defined}" ]
+    then
+        psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" <<-EOSQL
+            CREATE DATABASE $INTERMINE_USERS_DB OWNER $INTERMINE_USER;
+EOSQL
+    fi
+}
+
+
 main() {
-    create_chado_user
+    create_intermine_role_and_db
+    create_intermine_items_db
+    create_intermine_userprofile_db
 }
 
 main
