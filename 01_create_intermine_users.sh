6d01170976b6553e81250025b96289d0f8757f7c
Siddhartha Basu <siddhartha-basu@northwestern.edu>
2017-04-19 06:38:03 -0500

    Fixed the name of another environmental variable

--- 01_create_intermine_users.sh
+++ 01_create_intermine_users.sh
@@ -2,7 +2,7 @@
 set -xe
 
 create_intermine_role_and_db() {
-    if [ "${INTEMINE_USER+defined}" -a "${INTERMINE_PASSWORD+defined}" -a "${INTERMINE_DB+defined}" ]
+    if [ "${INTERMINE_USER+defined}" -a "${INTERMINE_PASSWORD+defined}" -a "${INTERMINE_DB+defined}" ]
     then
         psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" <<-EOSQL
             CREATE ROLE $INTERMINE_USER WITH CREATEDB LOGIN ENCRYPTED PASSWORD '$INTERMINE_PASSWORD';
@@ -12,7 +12,7 @@ EOSQL
 }
 
 create_intermine_items_db() {
-    if [ "${INTEMINE_USER+defined}" -a "${INTERMINE_PASSWORD+defined}" -a "${INTERMINE_ITEMS_DB+defined}" ]
+    if [ "${INTERMINE_USER+defined}" -a "${INTERMINE_PASSWORD+defined}" -a "${INTERMINE_ITEMS_DB+defined}" ]
     then
         psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" <<-EOSQL
             CREATE DATABASE $INTERMINE_ITEMS_DB OWNER $INTERMINE_USER;
@@ -21,7 +21,7 @@ EOSQL
 }
 
 create_intermine_userprofile_db() {
-    if [ "${INTEMINE_USER+defined}" -a "${INTERMINE_PASSWORD+defined}" -a "${INTERMINE_USERS_DB+defined}" ]
+    if [ "${INTERMINE_USER+defined}" -a "${INTERMINE_PASSWORD+defined}" -a "${INTERMINE_USERS_DB+defined}" ]
     then
         psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" <<-EOSQL
             CREATE DATABASE $INTERMINE_USERS_DB OWNER $INTERMINE_USER;
