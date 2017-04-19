d46ec694fdaddf4fbe4250ea1e9df1241257fefb
Siddhartha Basu <siddhartha-basu@northwestern.edu>
2017-04-19 06:26:46 -0500

    Fixed the issue with env variable name Added debugging

--- 01_create_intermine_users.sh
+++ 01_create_intermine_users.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
-set -e
+set -xe
 
 create_intermine_role_and_db() {
     if [ "${INTEMINE_USER+defined}" -a "${INTERMINE_PASSWORD+defined}" -a "${INTERMINE_DB+defined}" ]
     then
         psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" <<-EOSQL
-            CREATE ROLE $INTERMINE_USER WITH CREATEDB LOGIN ENCRYPTED PASSWORD '$INTERMINE_PASS';
+            CREATE ROLE $INTERMINE_USER WITH CREATEDB LOGIN ENCRYPTED PASSWORD '$INTERMINE_PASSWORD';
             CREATE DATABASE $INTERMINE_DB OWNER $INTERMINE_USER;
 EOSQL
     fi
